8d892d8d9d65c3ea6a2e1caa53165486658d5602
yuchao <18315391050@163.com>
2020-03-26 16:48:59 +0800

    test-c

---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  C1
+//
+//  Created by edz on 2020/3/26.
+//  Copyright © 2020 edz. All rights reserved.
+// 新建一个target后好像需要运行一次自带的程序，不然就是无本地化
+
+#include <stdio.h>
+
+int main() {
+
+    printf("* * * *\n");
+    printf(" * * *\n");
+    printf("  * *\n");
+    printf("   *\n");
+
+    
+    return 0;
+}
